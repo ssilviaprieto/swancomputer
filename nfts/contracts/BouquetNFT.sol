2c48a6875cf5b432b952beb30192bdf483ef01b5
astrovm <~@4st.li>
2025-08-29 23:31:37 -0300

    feat: Refactor BouquetNFT contract to remove Counters dependency and simplify token ID management

--- nfts/contracts/BouquetNFT.sol
+++ nfts/contracts/BouquetNFT.sol
@@ -3,19 +3,18 @@ pragma solidity ^0.8.29;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+// Note: OpenZeppelin v5 no longer provides Counters; we use a simple uint counter instead.
 
 // ERC-721 for the Bouquet artifact with public mint().
 // The same metadata URI is used for every token (edition-style).
 contract BouquetNFT is ERC721URIStorage, Ownable {
-    using Counters for Counters.Counter;
-
-    Counters.Counter private _tokenIdCounter;
+    uint256 private _tokenIdCounter;
     string public editionTokenURI;
     bool public mintOpen = true;
 
     constructor(string memory tokenUri) ERC721("Swan Bouquet", "SBOUQ") Ownable(msg.sender) {
         editionTokenURI = tokenUri;
+        _tokenIdCounter = 0;
     }
 
     function setMintOpen(bool open) external onlyOwner {
@@ -28,8 +27,8 @@ contract BouquetNFT is ERC721URIStorage, Ownable {
 
     function mint() external returns (uint256 tokenId) {
         require(mintOpen, "Mint closed");
-        _tokenIdCounter.increment();
-        tokenId = _tokenIdCounter.current();
+        _tokenIdCounter += 1;
+        tokenId = _tokenIdCounter;
         _safeMint(msg.sender, tokenId);
         _setTokenURI(tokenId, editionTokenURI);
     }
